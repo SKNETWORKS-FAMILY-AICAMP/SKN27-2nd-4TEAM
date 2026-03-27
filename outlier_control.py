27775d0c716844e66e3d9dc38d7b49cd0863cbb0
hjoo10200 <hjoo10200@gmail.com>
2026-03-28 01:28:05 +0900

    이상치 처리 함수 생성

---
+++ outlier_control.py
@@ -0,0 +1,19 @@
+def outlier_control(df,cols): 
+
+
+  for col in cols:
+    if col == 'CashbackAmount':
+      df[f'{col}_clip'] = df['CashbackAmount'].clip(lower=100,upper=240)
+
+    else:
+      df[f'{col}_log'] = np.log1p(df[col])
+
+
+  df.drop(cols, axis=1, inplace=True) # 원본 피처가 필요하면 이부분 주석처리하고 사용하세요
+
+
+
+
+cols=['Tenure','WarehouseToHome','DaySinceLastOrder','CashbackAmount']
+
+outlier_control(df,cols)
